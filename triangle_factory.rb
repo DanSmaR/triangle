6e91ac231f3ade22c2ce3ff8f27fd2aa0bb97e63
Danilo Martins <dansm86@outlook.com>
2024-07-30 21:23:02 -0300

    feat: implement scalene triangle area method

--- triangle_factory.rb
+++ triangle_factory.rb
@@ -1,10 +1,13 @@
 require_relative 'equilateral_triangle'
+require_relative 'scalene_triangle'
 
 class TriangleFactory
   def self.create_triangle(type, **kwargs)
     case type
     when 'equilateral'
       EquilateralTriangle.new(kwargs[:side])
+    when 'scalene'
+      ScaleneTriangle.new(kwargs[:side1], kwargs[:side2], kwargs[:side3])
     else
       raise ArgumentError, 'Unknown triangle type'
     end
